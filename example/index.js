0dce7d908a7a9abd72de45d78bc7a79d6e1eb982
wangchang <wangchang@didichuxing.com>
2019-02-28 18:41:30 +0800

    init

---
+++ example/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import MyComponent from '../src/index'
+
+const Demo = () => {
+    return <div><h1> 组件预览：</h1> <MyComponent /></div>
+}
+
+ReactDom.render( < Demo / > , document.getElementById('root'));
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import BookList from './booklist/booklist'
+import ReactApp from './reactdemo/App.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default ReactApp
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// export {
+//     BookList
+// }
